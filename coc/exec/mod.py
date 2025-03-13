07308bbce7aee2efbf2a5abdd87bab638febdc67
root <Ping J>
2025-03-13 20:42:07 +0800

    tro running w/o fail

--- coc/exec/mod.py
+++ coc/exec/mod.py
@@ -11,6 +11,7 @@ import os
 import logging
 from datetime import datetime
 import pickle
+import warnings
 
 from pydantic import Field
 
@@ -46,13 +47,17 @@ class Exec(BaseTool):
     def _run(self, code):
         """Execute code in a Jupyter-style way.
 
-           - All statements but the last are executed normally (exec).
-           - If the last statement is an expression, evaluate and print its result(s).
-           - If the last expression is comma-separated, each sub-expression is printed on its own line.
+        - All statements but the last are executed normally (exec).
+        - If the last statement is an expression, evaluate and print its result(s).
+        - If the last expression is comma-separated, each sub-expression is printed on its own line.
 
-           - returns (stdout, stderr)
+        - returns (stdout, stderr)
         """
+
         code_str = textwrap.dedent(code)
+
+        # remove trailing whitespace from each line
+        code_str = '\n'.join(line.rstrip() for line in code_str.split('\n'))
 
         old_stdout = sys.stdout
         old_stderr = sys.stderr
@@ -65,57 +70,53 @@ class Exec(BaseTool):
         errors = ''
 
         try:
-            # Parse code into an AST
-            parsed = ast.parse(code_str, mode='exec')
-            body = parsed.body
+            # Capture warnings during execution
+            with warnings.catch_warnings(record=True) as captured_warnings:
+                # Parse code into an AST
+                parsed = ast.parse(code_str, mode='exec')
+                body = parsed.body
 
-            # Separate all but the last statement from the last statement
-            *initial_statements, final_statement = body
+                # Separate all but the last statement from the last statement
+                *initial_statements, final_statement = body
 
-            # 1) Execute all statements but the last
-            if initial_statements:
-                initial_module = ast.Module(body=initial_statements, type_ignores=[])
-                compiled_init = compile(initial_module, filename='<ast>', mode='exec')
-                exec(compiled_init, self.globals)
+                # 1) Execute all statements but the last
+                if initial_statements:
+                    initial_module = ast.Module(body=initial_statements, type_ignores=[])
+                    compiled_init = compile(initial_module, filename='<ast>', mode='exec')
+                    exec(compiled_init, self.globals)
 
-            # 2) Jupyter-style evaluate/print for the last statement if it's an expression
-            if isinstance(final_statement, ast.Expr):
-                # final_statement.value is the actual expression AST node
-                last_expr = final_statement.value
+                # 2) Jupyter-style evaluate/print for the last statement if it's an expression
+                if isinstance(final_statement, ast.Expr):
+                    last_expr = final_statement.value
 
-                # If it's a tuple like '1, 2, 3', evaluate each item
-                if isinstance(last_expr, ast.Tuple):
-                    results = []
-                    for element in last_expr.elts:
-                        expr_code = ast.Expression(element)
+                    # If it's a tuple like '1, 2, 3', evaluate each item
+                    if isinstance(last_expr, ast.Tuple):
+                        results = []
+                        for element in last_expr.elts:
+                            expr_code = ast.Expression(element)
+                            compiled_expr = compile(expr_code, filename='<ast>', mode='eval')
+                            val = eval(compiled_expr, self.globals)
+                            results.append(val)
+                        # Print each result on a new line
+                        for r in results:
+                            if r is not None:
+                                print(r)
+                        self.globals['_'] = tuple(results)
+                    else:
+                        # Single expression
+                        expr_code = ast.Expression(last_expr)
                         compiled_expr = compile(expr_code, filename='<ast>', mode='eval')
                         val = eval(compiled_expr, self.globals)
-                        results.append(val)
-                    # Print each result on a new line
-                    for r in results:
-                        if r is not None:
-                            print(r)
-                    # Optionally, store the entire tuple in `_`
-                    self.globals['_'] = tuple(results)
-
+                        if val is not None:
+                            print(val)
+                            self.globals['_'] = val
                 else:
-                    # Single expression
-                    expr_code = ast.Expression(last_expr)
-                    compiled_expr = compile(expr_code, filename='<ast>', mode='eval')
-                    val = eval(compiled_expr, self.globals)
-                    if val is not None:
-                        print(val)
-                        # Store it so we can retrieve it from self.globals if needed
-                        self.globals['_'] = val
-
-            else:
-                # If the last statement is not an expression, just exec it
-                final_module = ast.Module(body=[final_statement], type_ignores=[])
-                compiled_final = compile(final_module, filename='<ast>', mode='exec')
-                exec(compiled_final, self.globals)
+                    # If the last statement is not an expression, just exec it
+                    final_module = ast.Module(body=[final_statement], type_ignores=[])
+                    compiled_final = compile(final_module, filename='<ast>', mode='exec')
+                    exec(compiled_final, self.globals)
 
         except Exception as e:
-            # If anything inside code execution raises an exception
             errors += str(e)
             traceback.print_exc(file=redirected_stderr)
 
@@ -125,16 +126,25 @@ class Exec(BaseTool):
         sys.stdout = old_stdout
         sys.stderr = old_stderr
 
-        if errors:
+        # Process captured warnings: filter for FutureWarnings
+        future_warnings = [fw for fw in captured_warnings if issubclass(fw.category, FutureWarning)]
+
+        # Log FutureWarnings and/or errors if either exists
+        if future_warnings or errors:
             with open(LOG_FILE, 'a') as f:
                 from datetime import datetime
                 f.write(f'{datetime.now().strftime("%m/%d %H:%M:%S")}\n')
                 f.write(code)
                 f.write('\n')
-                f.write('=' * 20)
-                f.write(errors)
-                traceback.print_exc(file=f)
-                f.write('\n\n')
+                if future_warnings:
+                    f.write('FutureWarnings:\n')
+                    for fw in future_warnings:
+                        f.write(str(fw.message) + '\n')
+                    f.write('\n')
+                if errors:
+                    f.write('Errors:\n')
+                    f.write(errors)
+                    f.write('\n')
 
         return (output, errors)
 
