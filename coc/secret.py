c6f552366550c4f9f8dd37162989638a708983e8
Your Name <jiang_kangping@outlook.com>
2025-03-16 13:04:58 +0000

    shhhhh

---
+++ coc/secret.py
@@ -0,0 +1,29 @@
+import os
+os.environ['TORCH_CUDA_ARCH_LIST'] = '8.9'
+
+os.environ['ANTHROPIC_API_KEY'] = 'sk-hNEOIT0d5C88QdjDTQPRUf5qwr6sZ8uik6do3t2liehumr49'
+os.environ['ANTHROPIC_BASE_URL'] = 'https://chat.cloudapi.vip/v1'
+
+os.environ['DEEPSEEK_API_KEY'] = 'sk-1f5e4b715d244fdaa68952be343ba4bd'
+os.environ['DEEPSEEK_BASE_URL'] = 'https://api.deepseek.com'
+
+
+os.environ['LANGCHAIN_API_KEY'] = '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'
+os.environ['LANGCHAIN_TRACING_V2'] = 'true'
+os.environ['LANGSMITH_TRACING'] = 'true'
+os.environ['LANGSMITH_ENDPOINT'] = 'https://api.smith.langchain.com'
+os.environ['LANGSMITH_API_KEY'] = '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'
+os.environ['LANGSMITH_PROJECT'] = 'zerobench'
+
+
+
+os.environ['OPENAI_API_KEY'] = 'sk-aDjzEoivIbVx4o9xLIEUDrpRaNTDOOhW1rTPhCsGsdjTa3Or'
+os.environ['OPENAI_API_BASE'] = 'https://chatapi.littlewheat.com/v1'
+
+os.environ['OPEN_ROUTER_API_KEY'] = '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'
+os.environ['OPEN_ROUTER_API_BASE'] = 'https://openrouter.ai/api/v1'
+
+os.environ['GEMINI_API_KEY'] = 'sk-ouw2SBfZp65CRS5gsclW356KYxcMeYPaUD1v8H0AeKsb8VqW'
+os.environ['GEMINI_BASE_URL'] = 'https://api.aigogo.top/v1'
+
+os.environ['DASHSCOPE_API_KEY'] = 'sk-3a88a7f6a9264d54b62eeb4181192248'
